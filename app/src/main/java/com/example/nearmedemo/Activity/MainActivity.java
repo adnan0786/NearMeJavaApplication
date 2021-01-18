eb47fc9662c5da4a91ea418f206d6fc662e41517
Muhammad Adnan <60383278+adnan0786@users.noreply.github.com>
2021-01-18 11:35:47 +0500

    Settings and user profile

--- app/src/main/java/com/example/nearmedemo/Activity/MainActivity.java
+++ app/src/main/java/com/example/nearmedemo/Activity/MainActivity.java
@@ -1,16 +1,113 @@
 package com.example.nearmedemo.Activity;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.example.nearmedemo.R;
+import com.example.nearmedemo.UserModel;
+import com.example.nearmedemo.databinding.ActivityMainBinding;
+import com.example.nearmedemo.databinding.NavDrawerLayoutBinding;
+import com.example.nearmedemo.databinding.ToolbarLayoutBinding;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private NavDrawerLayoutBinding navDrawerLayoutBinding;
+    private ActivityMainBinding activityMainBinding;
+    private ToolbarLayoutBinding toolbarLayoutBinding;
+    private FirebaseAuth firebaseAuth;
+    private CircleImageView imgHeader;
+    private TextView txtName, txtEmail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        navDrawerLayoutBinding = NavDrawerLayoutBinding.inflate(getLayoutInflater());
+        setContentView(navDrawerLayoutBinding.getRoot());
+        activityMainBinding = navDrawerLayoutBinding.mainActivity;
+        toolbarLayoutBinding = activityMainBinding.toolbar;
+
+        setSupportActionBar(toolbarLayoutBinding.toolbar);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this,
+                navDrawerLayoutBinding.navDrawer,
+                toolbarLayoutBinding.toolbar,
+                R.string.open_navigation_drawer,
+                R.string.close_navigation_drawer
+        );
+
+        navDrawerLayoutBinding.navDrawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavController navController = Navigation.findNavController(this, R.id.fragmentContainer);
+        NavigationUI.setupWithNavController(
+                navDrawerLayoutBinding.navigationView,
+                navController
+        );
+
+        View headerLayout = navDrawerLayoutBinding.navigationView.getHeaderView(0);
+
+        imgHeader = headerLayout.findViewById(R.id.imgHeader);
+        txtName = headerLayout.findViewById(R.id.txtHeaderName);
+        txtEmail = headerLayout.findViewById(R.id.txtHeaderEmail);
+
+        getUserData();
+
+
+    }
+
+    @Override
+    public void onBackPressed() {
+
+        if (navDrawerLayoutBinding.navDrawer.isDrawerOpen(GravityCompat.START))
+            navDrawerLayoutBinding.navDrawer.closeDrawer(GravityCompat.START);
+        else
+            super.onBackPressed();
+    }
+
+    private void getUserData() {
+        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("Users")
+                .child(firebaseAuth.getUid());
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                if (snapshot.exists()) {
+
+                    UserModel userModel = snapshot.getValue(UserModel.class);
+                    Glide.with(MainActivity.this).load(userModel.getImage()).into(imgHeader);
+                    txtName.setText(userModel.getUsername());
+                    txtEmail.setText(userModel.getEmail());
+
+
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/nearmedemo/Fragments/SettingsFragment.java
@@ -0,0 +1,254 @@
+package com.example.nearmedemo.Fragments;
+
+import android.Manifest;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.example.nearmedemo.Constant.AllConstant;
+import com.example.nearmedemo.Permissions.AppPermissions;
+import com.example.nearmedemo.R;
+import com.example.nearmedemo.Utility.LoadingDialog;
+import com.example.nearmedemo.databinding.FragmentSettingsBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.UserProfileChangeRequest;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class SettingsFragment extends Fragment {
+
+    private FragmentSettingsBinding binding;
+    private FirebaseAuth firebaseAuth;
+    private LoadingDialog loadingDialog;
+    private AppPermissions appPermissions;
+    private Uri imageUri;
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        binding = FragmentSettingsBinding.inflate(inflater, container, false);
+        firebaseAuth = FirebaseAuth.getInstance();
+        loadingDialog = new LoadingDialog(getActivity());
+        appPermissions = new AppPermissions();
+
+        binding.imgCamera.setOnClickListener(camera -> {
+
+            if (appPermissions.isStorageOk(getContext())) {
+                pickImage();
+            } else {
+                requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE
+                        , Manifest.permission.WRITE_EXTERNAL_STORAGE}, AllConstant.STORAGE_REQUEST_CODE);
+            }
+        });
+
+        binding.txtUsername.setOnClickListener(username -> {
+            usernameDialog();
+        });
+
+
+        return binding.getRoot();
+    }
+
+    private void pickImage() {
+
+        CropImage.activity()
+                .setCropShape(CropImageView.CropShape.OVAL)
+                .start(getContext(), this);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        ((AppCompatActivity) getActivity()).getSupportActionBar().setTitle("Settings");
+        binding.txtEmail.setText(firebaseAuth.getCurrentUser().getEmail());
+        binding.txtUsername.setText(firebaseAuth.getCurrentUser().getDisplayName());
+
+        Glide.with(requireContext()).load(firebaseAuth.getCurrentUser().getPhotoUrl()).into(binding.imgProfile);
+
+        if (firebaseAuth.getCurrentUser().isEmailVerified()) {
+            binding.txtEEmail.setVisibility(View.GONE);
+        } else {
+            binding.txtEEmail.setVisibility(View.VISIBLE);
+        }
+
+        binding.txtEEmail.setOnClickListener(verify -> {
+            firebaseAuth.getCurrentUser().sendEmailVerification().addOnCompleteListener(new OnCompleteListener<Void>() {
+                @Override
+                public void onComplete(@NonNull Task<Void> task) {
+                    if (task.isSuccessful()) {
+                        Toast.makeText(getContext(), "Mail sent verify the email", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(getContext(), "" + task.getException(), Toast.LENGTH_SHORT).show();
+                        Log.d("TAG", "onComplete: profile email " + task.getException());
+                    }
+                }
+            });
+        });
+
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == AllConstant.STORAGE_REQUEST_CODE) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                pickImage();
+            } else {
+                Toast.makeText(getContext(), "Storage Permission Denied", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+            if (resultCode == Activity.RESULT_OK) {
+                imageUri = result.getUri();
+                uploadImage(imageUri);
+            } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
+                Exception exception = result.getError();
+                Toast.makeText(getContext(), "" + exception.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private void uploadImage(Uri imageUri) {
+
+        loadingDialog.startLoading();
+        StorageReference storageReference = FirebaseStorage.getInstance().getReference();
+        storageReference.child(firebaseAuth.getUid() + AllConstant.IMAGE_PATH).putFile(imageUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+
+                Task<Uri> image = taskSnapshot.getStorage().getDownloadUrl();
+                image.addOnCompleteListener(new OnCompleteListener<Uri>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Uri> task) {
+                        if (task.isSuccessful()) {
+                            String url = task.getResult().toString();
+
+                            UserProfileChangeRequest profileChangeRequest = new UserProfileChangeRequest.Builder()
+                                    .setPhotoUri(Uri.parse(url))
+                                    .build();
+
+                            firebaseAuth.getCurrentUser().updateProfile(profileChangeRequest).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> profile) {
+
+                                    if (profile.isSuccessful()) {
+
+                                        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("Users");
+                                        Map<String, Object> map = new HashMap<>();
+                                        map.put("image", url);
+                                        databaseReference.child(firebaseAuth.getUid()).updateChildren(map);
+                                        Glide.with(requireContext()).load(url).into(binding.imgProfile);
+                                        loadingDialog.stopLoading();
+                                        Toast.makeText(getContext(), "Image Updated", Toast.LENGTH_SHORT).show();
+
+                                    } else {
+                                        loadingDialog.stopLoading();
+                                        Log.d("TAG", "Profile : " + profile.getException());
+                                        Toast.makeText(getContext(), "Profile : " + profile.getException(), Toast.LENGTH_SHORT).show();
+                                    }
+
+                                }
+                            });
+
+
+                        } else {
+                            loadingDialog.stopLoading();
+                            Toast.makeText(getContext(), "" + task.getException(), Toast.LENGTH_SHORT).show();
+                            Log.d("TAG", "onComplete: image url  " + task.getException());
+                        }
+
+                    }
+                });
+            }
+        });
+    }
+
+    private void usernameDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(requireContext());
+        View view = LayoutInflater.from(requireContext()).inflate(R.layout.username_dialog_layout, null, false);
+        builder.setView(view);
+        TextInputEditText edtUsername = view.findViewById(R.id.edtDialogUsername);
+
+        builder.setTitle("Edit Username");
+
+        builder.setPositiveButton("Save", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                String username = edtUsername.getText().toString().trim();
+                if (!username.isEmpty()) {
+
+                    UserProfileChangeRequest profileChangeRequest = new UserProfileChangeRequest.Builder()
+                            .setDisplayName(username)
+                            .build();
+                    firebaseAuth.getCurrentUser().updateProfile(profileChangeRequest).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if (task.isSuccessful()) {
+
+                                DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("Users");
+                                Map<String, Object> map = new HashMap<>();
+                                map.put("username", username);
+                                databaseReference.child(firebaseAuth.getUid()).updateChildren(map);
+
+                                binding.txtUsername.setText(username);
+                                Toast.makeText(getContext(), "Username is updated", Toast.LENGTH_SHORT).show();
+
+                            } else {
+                                Log.d("TAG", "onComplete: " + task.getException());
+                                Toast.makeText(getContext(), "" + task.getException(), Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                } else {
+                    Toast.makeText(getContext(), "Username is required", Toast.LENGTH_SHORT).show();
+                }
+            }
+        })
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                })
+                .create().show();
+    }
+}
